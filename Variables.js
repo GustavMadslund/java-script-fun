feb58181ec23e83da4a6af124ab83ed2ec429019
Gustav Madslund <gum@netcompany.com>
2021-02-13 20:45:54 +0100

    Add variable operations and interpolation.

--- Variables.js
+++ Variables.js
@@ -11,3 +11,40 @@ const entree = 'Enchiladas';
 console.log(entree);
 //entree = 'Tacos'; //TypeError
 //const testing; //SyntaxError
+
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+console.log('The value of levelUp:', levelUp);
+console.log('The value of powerLevel:', powerLevel);
+console.log('The value of multiplyMe:', multiplyMe);
+console.log('The value of quarterMe:', quarterMe);
+
+let gainedDollar = 3;
+let lostDollar = 50;
+
+gainedDollar++;
+lostDollar--;
+
+console.log(gainedDollar);
+console.log(lostDollar);
+
+let favoriteAnimal = 'dog';
+
+console.log('My favorite animal: ' + favoriteAnimal);
+
+//Interpolation
+let myName = 'Gustav Madslund';
+let myCity = 'New York';
+
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+
+
+
