3321e829a16c0daf20f5634161cc28488283dad8
Gustav Madslund <gum@netcompany.com>
2021-02-13 18:49:25 +0100

    Add hello world contents.

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,10 @@
 console.log('Hello World!');
+console.log('Hello' + 'World');
+console.log('Hello ' + 'World');
+console.log('Teaching the world how to code'.length);
+console.log('codecademy'.toUpperCase());
+console.log('    Remove whitespace    '.trim());
+console.log(Math.random() * 100);
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
