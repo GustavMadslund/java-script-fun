511366413b0b75c23cc6b63a6c6ea3ef8a215caf
Gustav Madslund <gum@netcompany.com>
2021-02-14 02:04:26 +0100

    Add intro to conditionals.

--- conditionals.js
+++ conditionals.js
@@ -23,3 +23,19 @@ if (mood === 'sleepy' && tirednessLevel > 8) {
 	console.log('not bed time yet');
 }
 
+let wordCount = 1;
+
+if (wordCount) {
+	console.log("Great! You've started your work!");
+} else {
+	console.log('Better get to work!');
+}
+
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+	console.log("This string doesn't seem to be empty.")
+} else {
+	console.log('This string is definitely empty.')
+}
+
