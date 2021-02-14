bc7fe6b6b9adcdbecb6e0f27a05d4e2cb9443eea
Gustav Madslund <gum@netcompany.com>
2021-02-14 12:21:23 +0100

    Add return;

--- functions.js
+++ functions.js
@@ -22,3 +22,12 @@ function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs') {
 }
 
 makeShoppingList();
+
+//Return
+function monitorCount(rows, columns) {
+	return rows * columns;
+}
+
+const numOfMonitors = monitorCount(5,4);
+
+console.log(numOfMonitors);
