6f29c7f9e2d08b2c2e4bcd718badd016476dc4b7
Gustav Madslund <gum@netcompany.com>
2021-02-14 02:06:32 +0100

    Add switch statements.

--- conditionals.js
+++ conditionals.js
@@ -71,3 +71,21 @@ if (season === 'spring') {
 	console.log('Invalid season.');
 }
 
+
+//Switch statement
+let athleteFinalPosition = 'first place';
+
+switch (athleteFinalPosition) {
+	case 'first place':
+		console.log('You get the gold medal!');
+		break;
+	case 'second place':
+		console.log('You get the silver medal!');
+		break;
+	case 'third place':
+		console.log('You get the bronze medal!');
+		break;
+	default:
+		console.log('No medal awarded.');
+		break;
+}
