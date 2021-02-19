b662f72bfe8f90bed7e179060c1e26e54749e12f
Gustav Madslund <gum@netcompany.com>
2021-02-19 20:32:33 +0100

    Add global scope

--- src/scope.js
+++ src/scope.js
@@ -7,3 +7,17 @@ function logCitySkyline() {
 
 console.log(logCitySkyline());
 
+//Global Scope
+let satellite = 'The Moon'
+let galaxy = 'The Milky Way'
+let stars = 'North Star'
+
+function callMyNightSky() {
+	return 'Night Sky: '
+		+ satellite + ', '
+		+ stars + ', and '
+		+ galaxy;
+}
+
+console.log(callMyNightSky());
+
