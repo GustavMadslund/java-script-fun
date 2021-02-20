835ad55f6f9d7dd127b44d9f8124b7054bc0649f
Gustav Madslund <gum@netcompany.com>
2021-02-20 15:40:56 +0100

    Add arrays.

--- src/arrays/arrays.js
+++ src/arrays/arrays.js
@@ -9,3 +9,31 @@ let listItem = famousSayings[0];
 console.log(listItem); 
 console.log(famousSayings[2]);
 console.log(famousSayings[3]); //undefined
+
+//Update Elements
+let groceryList = ['bread', 'tomatoes', 'milk'];
+groceryList[1] = 'avocados';
+console.log(groceryList);
+
+//Let and const arrays
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+console.log(condiments);
+
+condiments = ['Mayo'];
+console.log(condiments);
+
+utensils[3] = 'Spoon';
+console.log(utensils);
+
+//length
+const objectives = ['Learn a new language', 'Read 52 books', 'Run a marathon'];
+
+console.log(objectives.length);
+
+//Nested arrays
+let numberClusters = [[1,2],[3,4],[5,6]];
+const target = numberClusters[2][1];
+console.log(target);
