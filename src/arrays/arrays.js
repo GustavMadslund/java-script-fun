7bea538daaa573086b40558bd8cbf536b476e221
Gustav Madslund <gum@netcompany.com>
2021-02-20 14:35:37 +0100

    Add arrays.

---
+++ src/arrays/arrays.js
@@ -0,0 +1,11 @@
+const hobbies = ['JavaScript', 'Go', 'Ruby'];
+
+console.log(hobbies);
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+let listItem = famousSayings[0];
+
+console.log(listItem); 
+console.log(famousSayings[2]);
+console.log(famousSayings[3]); //undefined
