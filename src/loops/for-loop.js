d467aff18d27ba18e12a21a590be629702bff084
Gustav Madslund <gum@netcompany.com>
2021-02-24 19:22:57 +0100

    Add loop break.

--- src/loops/for-loop.js
+++ src/loops/for-loop.js
@@ -16,3 +16,13 @@ for (let i = 0; i < vacationSpots.length; i++) {
 	console.log('I would love to visit ' + vacationSpots[i]);
 }
 
+//Break
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+for (let i = 0; i < rapperArray.length; i++) {
+	console.log(rapperArray[i]);
+	if (rapperArray[i] === 'Notorious B.I.G.') {
+		break;
+	}
+}
+console.log("And if you don't know, now you know.");
