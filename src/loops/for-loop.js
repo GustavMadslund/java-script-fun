72c65afef2379bcb32890268842303bb278d9114
Gustav Madslund <gum@netcompany.com>
2021-02-24 19:11:30 +0100

    Add for loop.

---
+++ src/loops/for-loop.js
@@ -0,0 +1,18 @@
+const vacationSpots = ['New York', 'Barcelona', 'Los Angeles'];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+for (let counterOne = 5; counterOne <= 10; counterOne++) {
+	console.log(counterOne);
+}
+
+for (let counterTwo = 3; counterTwo >= 0; counterTwo--) {
+	console.log(counterTwo);
+}
+
+for (let i = 0; i < vacationSpots.length; i++) {
+	console.log('I would love to visit ' + vacationSpots[i]);
+}
+
